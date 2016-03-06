d3892f3cfc7ee585fb8734995a237efe31eec692
Sebastian Hallén <sebastian.hallen@tretton37.com>
2016-03-07 00:35:16 +0100

    some hard coded replacements for prebenizer

--- assets/prebenize.js
+++ assets/prebenize.js
@@ -1,4 +1,4 @@
-var pipeline = [ toDanishChars ];
+var pipeline = [ commonReplacements ];
 
 function toDanishChars(input) {
   return input
@@ -6,6 +6,19 @@ function toDanishChars(input) {
     .replace(/Ä/g, 'Æ')
     .replace(/ö/g, 'ø')
     .replace(/Ö/g, 'Ø');
+}
+
+function commonReplacements(input) {
+  return input
+    .replace(/(^|\s)([vV])ad($|\s)/g, '$1$2äd$3')
+    .replace(/(^|\s)att($|\s)/g, '$1ätt$2')
+    .replace(/(^|\s)([jJ])ag($|\s)/g, '$1$2ej$3')
+    .replace(/(^|\s)([sS])ka($|\s)/g, '$1$2kä$3')
+    .replace(/(^|\s)([iI])nte($|\s)/g, '$1$2ge$3')
+    .replace(/(^|\s)([aA])lla($|\s)/g, '$1$2lle$3')
+    .replace(/(^|\s)([nN])u($|\s)/g, '$1$2ou$3')
+    .replace(/(^|\s)([aÄ])r($|\s)/g, '$1$2er$3')
+    .replace(/(^|\s)(kaka|fika)($|\s)/gi, '$1wienerbrö$3');
 }
 
 function prebenize(input) {
